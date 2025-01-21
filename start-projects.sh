9d5d5a60f7d2be9833f1215771b350a66fedff01
Roberto Sanchez <roberto.sanchez.reolid@gmail.com>
2025-01-21 22:03:08 +0100

    Initial commit

---
+++ start-projects.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Navigate to the Astro project directory and start the development server
+cd /home/robert/personalwebastro/astroweb && npm run dev &
+
+# Navigate to the Strapi project directory and start the development server
+cd /home/robert/personalwebastro/strapi-cms && npm run develop
